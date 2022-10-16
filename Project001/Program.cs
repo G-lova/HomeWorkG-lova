18b2b32ac5d4e25f277b1ba061d2b1246af8fbcb
Lyubov Gavrilova <suslik.lyuba@gmail.com>
2022-10-16 22:31:54 +0300

    добавлено решение задачи 6

--- Project001/Program.cs
+++ Project001/Program.cs
@@ -1,5 +1,6 @@
 ﻿//ДЗ 1
 //Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
 /*
 Console.Write("Введите первое число ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
@@ -24,6 +25,7 @@ else
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+/*
 Console.Write("Введите первое число ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число ");
@@ -40,3 +42,19 @@ if (thirdNumber > max)
     max = thirdNumber;
 }
 Console.WriteLine($"Максимальное число {max}");
+*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int even = number % 2;
+if (even == 0)
+{
+Console.WriteLine($"Число {number} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {number} является НЕчетным");
+}

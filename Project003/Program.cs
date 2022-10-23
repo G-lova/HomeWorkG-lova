3e57e7cd66ce700f05945332fee34044cf523cdb
Lyubov Gavrilova <suslik.lyuba@gmail.com>
2022-10-23 18:46:04 +0300

    добавлено решение задачи 21

--- Project003/Program.cs
+++ Project003/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
+/*
 bool NumPalindrome(int num)
 {
     bool result = false;
@@ -33,3 +34,31 @@ bool NumPalindrome(int num)
 Console.Write("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(NumPalindrome(number));
+*/
+
+//Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double Distance3d (double xa, double xb, double ya, double yb, double za, double zb)
+{
+    double sqrX = (xb - xa) * (xb - xa);
+    double sqrY = (yb - ya) * (yb - ya);
+    double sqrZ = (zb - za) * (zb - za);
+    double distance = Math.Sqrt(sqrX + sqrY + sqrZ);
+    return distance;
+}
+
+Console.Write("Введите координату X первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z первой точки: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату X второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z второй точки: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double result = Distance3d(x1,x2,y1,y2,z1,z2);
+Console.WriteLine($"Расстояние между двумя пространственными точками: {result}");

d42017e88fc5509e6733106d24de9c63eb280cac
Lyubov Gavrilova <suslik.lyuba@gmail.com>
2022-11-09 21:10:02 +0300

    добавлено решение задачи 29

--- Project005/Program.cs
+++ Project005/Program.cs
@@ -16,7 +16,7 @@ Console.WriteLine($"Число {firstNum} в степени {secondNum} равн
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-
+/*
 int SumFigures(int number)
 {
     int sumFigures = 0;
@@ -34,3 +34,31 @@ int SumFigures(int number)
 Console.Write("Введите число ");
 int num = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Сумма цифр числа {num} равна {SumFigures(num)}");
+*/
+
+// Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите элемент массива: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateArray(m);
+ShowArray(myArray);

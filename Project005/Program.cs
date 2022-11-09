6fe0c5f81628a55ce1f4de65512773ff0c147d5a
Lyubov Gavrilova <suslik.lyuba@gmail.com>
2022-11-09 21:08:33 +0300

    добавлено решение задачи 27

--- Project005/Program.cs
+++ Project005/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-
+/*
 double NumberDegree(double firstNumber, double secondNumber)
 {
     double degree = Math.Pow(firstNumber, secondNumber);
@@ -12,3 +12,25 @@ double firstNum = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите степень числа ");
 double secondNum = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine($"Число {firstNum} в степени {secondNum} равно {NumberDegree(firstNum, secondNum)}");
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+int SumFigures(int number)
+{
+    int sumFigures = 0;
+    int digitFigure = 0;
+
+    while (number > 0)
+    {
+        digitFigure = number % 10;
+        sumFigures += digitFigure;
+        number = number / 10;
+    }
+    return sumFigures;
+}
+
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр числа {num} равна {SumFigures(num)}");

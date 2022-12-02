52be9f367b2ba85f9d2905eb1ea2d614234891b1
Lyubov Gavrilova <suslik.lyuba@gmail.com>
2022-12-02 18:26:10 +0300

    добавлено решение задачи 66

--- Project009/Program.cs
+++ Project009/Program.cs
@@ -4,6 +4,7 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+/*
 void ShowNums(int n)
 {
     Console.Write(n + " ");
@@ -13,12 +14,27 @@ void ShowNums(int n)
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 ShowNums(n);
+*/
 
 // Задача 66: Задайте значения M и N. Напишите программу,
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+int SumOfDiapasonNumbers(int m, int n)
+{
+    if (m < n) return SumOfDiapasonNumbers(m, n - 1) + n;
+    else
+        if (m > n) return SumOfDiapasonNumbers(m - 1, n) + m;
+    return m;
+}
+
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма элементов в промежутке от {m} до {n} равна {SumOfDiapasonNumbers(m, n)}");
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
 // Даны два неотрицательных числа m и n.
